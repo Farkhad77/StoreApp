e27d49827af0bee82deb30b8f851fd548c7b38f5
Farkhad77 <farhadramazanov2006@gmail.com>
2025-07-02 13:04:41 +0400

    Repository Created

---
+++ src/Core/StoreApp.Application/Abstracts/Repositories/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using StoreApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Application.Abstracts.Repositories;
+public interface IRepository<T> where T : BaseEntity, new()
+{
+    Task<T?> GetByIdAsync(Guid id);
+    IQueryable<T> GetByFiltered(Expression<Func<T, bool>>? predicate = null,
+        Expression<Func<T, object>>[]? include = null,
+        bool isTracking = false);
+    IQueryable<T> GetAll(bool isTracking = false);
+
+    IQueryable<T> GetAllFiltered(Expression<Func<T, bool>>? predicate = null,
+        Expression<Func<T, object>>[]? include = null,
+        Expression<Func<T, bool>>? orderBy = null,
+        bool isOrderByAsc = true,
+        bool isTracking = false);
+    Task SaveChangeAsync();
+    Task AddAsync(T entity);
+    Task<List<T>> GetByNameSearchAsync(string namePart, Expression<Func<T, string>> selector);
+    void Update(T entity);
+    void Delete(T entity);
+
+}
