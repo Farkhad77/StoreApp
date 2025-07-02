24aa9fdb548cccad2fa2de1c048f21f7b6e0f76a
Farkhad77 <farhadramazanov2006@gmail.com>
2025-07-02 21:05:05 +0400

    Base Response Added

---
+++ src/Core/StoreApp.Application/Shared/BaseResponse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Application.Shared
+{
+    public class BaseResponse<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T Data { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+
+
+        public BaseResponse(HttpStatusCode statusCode)
+        {
+            Success = true;
+            StatusCode = statusCode;
+        }
+        public BaseResponse(string message, HttpStatusCode statusCode)
+        {
+            Message = message;
+            Success = false;
+            StatusCode = statusCode;
+        }
+        public BaseResponse(string message, bool isSuccess, HttpStatusCode statusCode)
+        {
+            Message = message;
+            Success = isSuccess;
+            StatusCode = statusCode;
+        }
+        public BaseResponse(string message, T? data, HttpStatusCode statusCode)
+        {
+            Success = true;
+            Message = message;
+            Data = data;
+            StatusCode = statusCode;
+        }
+    }
+}
