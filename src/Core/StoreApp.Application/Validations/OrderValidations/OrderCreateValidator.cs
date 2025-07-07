d65c152075b3f654922317b27f292fff4946ec28
Farkhad77 <farhadramazanov2006@gmail.com>
2025-07-07 13:36:53 +0400

    validations

---
+++ src/Core/StoreApp.Application/Validations/OrderValidations/OrderCreateValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using StoreApp.Application.DTOs.OrderDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Application.Validations.OrderValidations
+{
+    public class OrderCreateDtoValidator : AbstractValidator<OrderCreateDto>
+    {
+        public OrderCreateDtoValidator()
+        {
+            RuleFor(x => x.ProductIds)
+                .NotNull().WithMessage("Məhsul siyahısı boş ola bilməz.")
+                .Must(ids => ids != null && ids.Any()).WithMessage("Məhsul siyahısı boş ola bilməz.");
+
+            RuleFor(x => x.OrderCount)
+                .GreaterThan(0).WithMessage("Sifariş sayı sıfırdan böyük olmalıdır.");
+        }
+    }
+}
